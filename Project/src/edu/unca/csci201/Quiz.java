92fb74d425d7ca7176a0a8f96f5b7373a3425799
Bren <Bren@10.120.165.24>
2017-11-29 15:25:44 -0500

    Update

--- Project/src/edu/unca/csci201/Quiz.java
+++ Project/src/edu/unca/csci201/Quiz.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Quiz {
 
 	//creates an array of 25 questions
-	Question [] listOfQuestions;
-
+	private Question [] listOfQuestions;
+	
 	//constructor
 	public Quiz () {
 		listOfQuestions = new Question [25];
 	}
 
 	int currentQuestion = 0;
-	
+
 	public void addQuestion(Question q) {
 
 		listOfQuestions[currentQuestion] = q;
@@ -21,7 +21,7 @@ public class Quiz {
 	}
 
 	double grade = 0;
-	
+
 	public void giveQuiz() {
 		System.out.println("Rules of the Quiz: For Multiple Choice Questions type Capital Letter, For Short Answer and True/False answer must start with a capital letter! Good Luck!");
 		System.out.println();
@@ -29,14 +29,29 @@ public class Quiz {
 
 		for (int i=0; i<25; i++) {
 			System.out.println(listOfQuestions[i].getQuestion());
-			
-			String userAnswer = scan.next();
-			
+
+			String userAnswer = scan.nextLine();
+
 			if (listOfQuestions[i].isCorrectAnswer(userAnswer))
 				grade++;
 		}
+		
+		double score = grade/25;
+		System.out.println("Your score on this quiz was: " + score);
+		if (score > 0.90) {
+			System.out.println("Awesome Job! You got an A!!");
+		} else if (score > 0.8 && score < 0.9) {
+			System.out.println("Good Job! You got an B!");
+		} else if (score > 0.7 && score < 0.8) {
+			System.out.println("Oh dear, you got an C.");
+		} else if (score > 0.6 && score < 0.7) {
+			System.out.println("Well, you got an D.");
+		}else if (score < 0.6) {
+			System.out.println("FAIL! You got an F!!");
 
-		System.out.println("Your score on this quiz was: " + grade/25);
-		scan.close();
-	}
+			scan.close();
+		}
+	}	
 }
+
+
